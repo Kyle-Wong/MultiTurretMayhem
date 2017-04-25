a5103dc51d64c0b779731b6355835245c4e6e340
thetylerace13 <tylergemora@yahoo.com>
2017-04-24 20:48:30 -0700

    Can now control left and right spawn rates separately.

---
+++ MultiTurretMayhem/Assets/HoldSpawners.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldSpawners : MonoBehaviour {
+    public Transform left;
+    public Transform right;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+}
--- MultiTurretMayhem/Assets/Scripts/Spawning.cs
+++ MultiTurretMayhem/Assets/Scripts/Spawning.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawning : MonoBehaviour {
+    public Transform target;
+    public float offset;
     public List<GameObject> objects;
     public List<float> chances;
     public float generalRate;
@@ -20,24 +22,26 @@ public class Spawning : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        transform.localPosition = new Vector3(offset, 0);
+
         spawnTimer += Time.deltaTime;
 
         generalRate += deltaGeneralRate * Time.deltaTime / 60;
 
         if (spawnTimer > 60 / generalRate)
         {
-            Vector3 spawnPosition = transform.position + new Vector3(posOrNeg(range), Random.Range(-1.0f, 1.0f) * range * 9 / 16);
-            Instantiate(pickRandom(), spawnPosition, Quaternion.LookRotation(Vector3.forward, transform.position - spawnPosition));
+            Vector3 spawnPosition = transform.position + new Vector3(0, Random.Range(-1.0f, 1.0f) * range);
+            Instantiate(pickRandom(), spawnPosition, Quaternion.LookRotation(Vector3.forward, target.position - spawnPosition));
             spawnTimer = 0;
         }
 	}
 
-    private float posOrNeg(float f)
-    {
-        if (Random.Range(0, 2) == 1)
-            return -f;
-        return f;
-    }
+    //private float posOrNeg(float f)
+    //{
+    //    if (Random.Range(0, 2) == 1)
+    //        return -f;
+    //    return f;
+    //}
 
     private GameObject pickRandom()
     {
