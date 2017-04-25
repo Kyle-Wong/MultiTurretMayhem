b8e5810351f36db2ed7d8e060d03a8d28f5f4dbb
Jake Smith <jesmith24@comcast.net>
2017-04-24 20:34:06 -0700

    Asteroid works

---
+++ MultiTurretMayhem/Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void death()
+    {
+        Destroy(this.gameObject);
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Turret")) //ToDo: check if this is actually the tag for the player
+        {
+            death();
+        }
+    }
+}
---
+++ MultiTurretMayhem/Assets/Scripts/Enemies/Simple_Movement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Simple_Movement : MonoBehaviour {
+
+    public float velocity;
+
+    //Base size for your object
+    public float size;
+
+    // Change this from 0 if you want sizes to be different per spawn.
+    public float sizeDeviation = 0;
+
+
+    // Use this for initialization
+    void Start () {
+
+        //Velocity will be set to move towards the center
+        var dir = new Vector3(0,0,0) - transform.position;
+
+        this.GetComponent<Rigidbody2D>().velocity =  dir.normalized * velocity;
+        float newSize = size + (Random.Range(0f, 1f) * sizeDeviation);
+        this.transform.localScale = new Vector3(newSize, newSize, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
