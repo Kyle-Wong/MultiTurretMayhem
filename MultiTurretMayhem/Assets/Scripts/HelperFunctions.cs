d3983a529ea0ff6ebd807f4e3e7dbe1cba2cd7b3
thetylerace13 <tylergemora@yahoo.com>
2017-04-27 22:11:15 -0700

    Spawner changes: when an enemy spawns, it spawns either left or right (so non simultaenous enemy spawns), spawning occurs in a circular sphere range now (left and right still cutomizable), and can create custom angles for spawning.

--- MultiTurretMayhem/Assets/Scripts/HelperFunctions.cs
+++ MultiTurretMayhem/Assets/Scripts/HelperFunctions.cs
@@ -16,11 +16,31 @@ public class HelperFunctions{
         return -1;
     }
 
+    public static int randomIndex(List<Vector2> probabilities) // Pick a random index in a list of vector2 weighted by the contents in the list
+    {
+        float selection = Random.Range(0.0f, sumOfList(probabilities));
+        for (int i = 0; i < probabilities.Count; ++i)
+        {
+            if (selection < probabilities[i].y - probabilities[i].x)
+                return i;
+            selection -= probabilities[i].y - probabilities[i].x;
+        }
+        return -1;
+    }
+
     public static float sumOfList(List<float> list) // Return the sum of a list of floats
     {
         float result = 0.0f;
         foreach (float f in list)
             result += f;
+        return result;
+    }
+
+    public static float sumOfList(List<Vector2> list) // Return the sum of a list of Vector2 (if the vector2 is a range)
+    {
+        float result = 0.0f;
+        foreach (Vector2 v in list)
+            result += v.y - v.x;
         return result;
     }
 
--- MultiTurretMayhem/Assets/Scripts/Spawning.cs
+++ MultiTurretMayhem/Assets/Scripts/Spawning.cs
@@ -3,20 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawning : MonoBehaviour {
-    public float offset;                //Where in relation to spawner object (horizontally) this spawner is
+    public Transform target;            //The target to aim at
     public List<GameObject> objects;    //Which objects to spawn
     public List<float> chances;         //Which probabilties (respectively) at which the objects spawn
-    public float generalRate;           //The starting rate at which objects generally spawn (objects per minute)
-    public float deltaGeneralRate;      //The change over time in which objects generally spawn (objects per minute squared)
-    public float range;                 //The vertical range in which objects can spawn in
+    public float leftRate;              //The starting rate at which objects generally spawn on the left side (objects per minute)
+    public float deltaLeftRate;         //The change over time in which objects generally spawn on the left side(objects per minute squared)
+    public List<Vector2> leftAngles;          //The angle range at which the object can spawn on the left side (degrees)
+    public float rightRate;             //The starting rate at which objects generally spawn on the right side (objects per minute)
+    public float deltaRightRate;        //The change over time in which objects generally spawn on the right side(objects per minute squared)
+    public List<Vector2> rightAngles;         //The angle range at which the object can spawn on the right side (degrees)
+    public float distance;              //The distance at which objects spawn from
 
-    private Targetting targetter;
     private float spawnTimer = 0.0f;
-
-    void Awake()
-    {
-        targetter = GetComponentInParent<Targetting>();
-    }
 
     // Use this for initialization
     void Start ()
@@ -27,29 +25,41 @@ public class Spawning : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        transform.localPosition = new Vector3(offset, 0);
-
         spawnTimer += Time.deltaTime;
 
-        generalRate += deltaGeneralRate * Time.deltaTime / 60; // Rate changes over time
+        leftRate += deltaLeftRate * Time.deltaTime / 60; //Rate changes over time
+        rightRate += deltaRightRate * Time.deltaTime / 60; //Rate changes over time
 
-        if (spawnTimer > 60 / generalRate) // Spawn object
-        {
-            Vector3 spawnPosition = transform.position + new Vector3(0, Random.Range(-1.0f, 1.0f) * range);
-            Instantiate(pickRandom(), spawnPosition, Quaternion.LookRotation(Vector3.forward, targetter.target.position - spawnPosition));
-            spawnTimer = 0;
-        }
+        float generalRate = leftRate + rightRate;
+
+        if (spawnTimer > 60 / generalRate)
+            spawnObject();
 	}
 
-    //private float posOrNeg(float f) // Return a positive or negative version of the float randomly
+    //private float posOrNeg(float f) //Return a positive or negative version of the float randomly
     //{
     //    if (Random.Range(0, 2) == 1)
     //        return -f;
     //    return f;
     //}
 
-    private GameObject pickRandom() // Pick a random object from the objects list using the chances list
+    private GameObject pickRandom() //Pick a random object from the objects list using the chances list
     {
         return objects[HelperFunctions.randomIndex(chances)];
+    }
+
+    private void spawnObject() //Spawn a game object
+    {
+        float spawnAngle = 0.0f;
+        Vector2 angleRange;
+        if (Random.Range(0.0f, leftRate + rightRate) < leftRate)
+            angleRange = leftAngles[HelperFunctions.randomIndex(leftAngles)];
+        else
+            angleRange = rightAngles[HelperFunctions.randomIndex(rightAngles)];
+        spawnAngle = Random.Range(angleRange.x, angleRange.y);
+
+        Vector3 spawnPosition = transform.position + HelperFunctions.lineVector(spawnAngle, distance);
+        Instantiate(pickRandom(), spawnPosition, Quaternion.LookRotation(Vector3.forward, target.position - spawnPosition));
+        spawnTimer = 0;
     }
 }
