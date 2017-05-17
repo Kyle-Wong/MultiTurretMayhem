441f44b670d154cdbba5be5a872205a7bd79f617
Kyle Wong <kylewonga@gmail.com>
2017-05-16 18:51:45 -0700

    Added comet and twister sprites

--- MultiTurretMayhem/Assets/Scripts/Enemies/Twister.cs
+++ MultiTurretMayhem/Assets/Scripts/Enemies/Twister.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public class Twister : Enemy
 {
     public float tanSpeed = 10;
-
+    private Transform player;
+    private int direction;
     // Use this for initialization
     void Start()
     {
         baseStart();
         baseHP = 1;
         curHP = 1;
-        Debug.Log(tanSpeed);
         GetComponent<Simple_Movement>().tanVelocity = HelperFunctions.friendlySqrt(Random.Range(-Mathf.Pow(tanSpeed, 2), Mathf.Pow(tanSpeed, 2)));
+        direction = GetComponent<Simple_Movement>().tanVelocity > 0 ? 0 : 1;
+        GetComponentInChildren<SpriteRenderer>().transform.Rotate(0,0,180 * direction);
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
         baseUpdate();
+        transform.rotation = Quaternion.LookRotation(Vector3.back, transform.position - player.position);
     }
 }
