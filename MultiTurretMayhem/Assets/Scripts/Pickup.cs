b4f6e88309d1dafc18f191484a03aba55dc909a2
thetylerace13 <tylergemora@yahoo.com>
2017-05-17 17:50:40 -0700

    Pickups have a pulse to make them visually stand out, and they put out a circle upon getting shot

--- MultiTurretMayhem/Assets/Scripts/Pickup.cs
+++ MultiTurretMayhem/Assets/Scripts/Pickup.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Pickup : MonoBehaviour {
     public int healthAdd;
     public int bombAdd;
+    public Color effectColor;
     private const int MAX_HEALTH = 8;
     private const int MAX_BOMBS = 5;
     private gameController ctrl;
+    private float pulseTime = 2;
+    private float pulseTimer = 0.0f;
 
     void Awake()
     {
@@ -17,21 +20,48 @@ public class Pickup : MonoBehaviour {
     // Use this for initialization
     void Start ()
     {
-
+        StartCoroutine(advancePulserTimer());
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
+		if (pulseTimer >= pulseTime)
+        {
+            pulseTimer = 0;
+            Pulse();
+            StartCoroutine(advancePulserTimer());
+        }
 	}
+
+    public void Pulse()
+    {
+        GameObject pulse = (GameObject)Instantiate(Resources.Load("PulseBomb"), transform.position, Quaternion.identity);
+        ColorLerp bombColor = pulse.transform.GetChild(0).gameObject.GetComponent<ColorLerp>();
+        bombColor.startColor = effectColor;
+        bombColor.endColor = new Color(effectColor.r, effectColor.g, effectColor.b, 0);
+    }
 
     public void apply()
     {
+        GameObject effectBomb = (GameObject)Instantiate(Resources.Load("PickupBomb"), transform.position, Quaternion.identity);
+        ColorLerp bombColor = effectBomb.transform.GetChild(0).gameObject.GetComponent<ColorLerp>();
+        bombColor.startColor = effectColor;
+        bombColor.endColor = new Color(effectColor.r, effectColor.g, effectColor.b, 0);
+
         if(ctrl.bombs < MAX_BOMBS)
             ctrl.bombs += bombAdd;
         if(ctrl.getHealth() < MAX_HEALTH)
             ctrl.setHealth(ctrl.getHealth() + healthAdd);
         Destroy(gameObject);
+    }
+
+    private IEnumerator advancePulserTimer()
+    {
+        while (pulseTimer < pulseTime)
+        {
+            yield return new WaitForEndOfFrame();
+            pulseTimer += Time.deltaTime;
+        }
     }
 }
