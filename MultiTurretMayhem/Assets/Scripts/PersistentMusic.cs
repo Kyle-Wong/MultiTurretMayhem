0e53d53f7ab964ca5eeba3f266bb62dc969ad1e1
Tyler Gemora <tgemora@uci.edu>
2017-06-02 13:20:15 -0700

    Persistent music object changed

---
+++ MultiTurretMayhem/Assets/Scripts/PersistentMusic.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentMusic : MonoBehaviour {
+    private AudioSource musicSource;
+    public List<AudioClip> musicList;
+
+    private void Awake()
+    {
+        musicSource = GetComponent<AudioSource>();
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        StartCoroutine(playSongs());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    private IEnumerator playSongs()
+    {
+        while (true)
+        {
+            AudioClip song = musicList[Random.Range(0, musicList.Count)];
+            HelperFunctions.playSound(ref musicSource, song);
+            yield return new WaitForSeconds(song.length);
+        }
+    }
+}
