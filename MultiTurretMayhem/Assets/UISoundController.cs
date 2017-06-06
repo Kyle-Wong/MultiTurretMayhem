804abda560afa54b166433e90df53f202c7b75a7
Kyle Wong <kylewonga@gmail.com>
2017-06-05 20:29:42 -0700

    Added UI sounds to campaign

---
+++ MultiTurretMayhem/Assets/UISoundController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UISoundController : MonoBehaviour {
+
+    // Use this for initialization
+    public AudioClip selectSound;
+    public AudioClip confirmSound;
+    private AudioSource source;
+	void Awake () {
+        source = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void playSelectSound()
+    {
+        source.clip = selectSound;
+        source.Play();
+    }
+    public void playConfirmSound()
+    {
+        source.clip = confirmSound;
+        source.Play();
+    }
+
+}
